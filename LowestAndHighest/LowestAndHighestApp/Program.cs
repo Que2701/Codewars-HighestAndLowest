f71e60d1f318e7621d6dee7c41c8971d0d69e544
tumelo <tumelom@evolvemed.co.za>
2019-09-26 09:58:52 +0200

    highest and lowest integers

---
+++ LowestAndHighest/LowestAndHighestApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowestAndHighestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string highestLowestString = "1 9 3 4 -5";
+            Console.WriteLine(HighestLowestNumbers(highestLowestString));
+            Console.WriteLine(HighAndLow(highestLowestString));
+        }
+
+        static string HighestLowestNumbers(string highestLowestNumbers)
+        {
+            // split the string into an aray of chars
+            string[] characterDelimiter = { Environment.NewLine, " "};
+            int[] numbers = highestLowestNumbers.Split(characterDelimiter, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => Int32.Parse(x)).ToArray();
+            Array.Sort(numbers);
+
+            return highestLowestNumbers = numbers.Last() + " " + numbers.First().ToString();
+        }
+
+        static string HighAndLow(string numbers)
+        {
+            var parsedNumbers = numbers.Split().Select(int.Parse);
+            return $"{parsedNumbers.Max()} {parsedNumbers.Min()}";
+        }
+    }
+}
